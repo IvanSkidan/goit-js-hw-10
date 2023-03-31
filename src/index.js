26010cf00f759d42c895722f7224637e43fcc30a
Ivan Skidan <vanyalife7@gmail.com>
2023-03-31 20:53:36 +0300

    updated the files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputBox = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+// inputBox.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
